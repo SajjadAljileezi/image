f3d7d705ade6eb354200fdc9e0ff24998f90c137
Sajjad Aljileezi <30444995+SajjadAljileezi@users.noreply.github.com>
2020-05-21 12:33:00 -0700

    This is a new commit for what I originally planned to be amended

---
+++ app/Http/Controllers/ImagesController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+use Image;
+use App\images;
+use Illuminate\Http\Request;
+
+class ImagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $img = Image::make('home.jpg')->greyscale();
+
+    return $img->response('jpg');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\images  $images
+     * @return \Illuminate\Http\Response
+     */
+    public function show(images $images)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\images  $images
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(images $images)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\images  $images
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, images $images)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\images  $images
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(images $images)
+    {
+        //
+    }
+}

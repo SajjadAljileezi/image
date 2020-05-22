e10ef1a49aea779b516dbd6b26df22a8cf14a076
Sajjad Aljileezi <30444995+SajjadAljileezi@users.noreply.github.com>
2020-05-21 20:18:26 -0700

    second commit
    setup work env

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/image', 'ImagesController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
